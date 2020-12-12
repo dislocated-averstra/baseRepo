af13c82cbe236b4666a71128d9d5ee81c750695e
bichn <caleb.klab@gmail.com>
2020-12-11 19:47:47 -0600

    caleb update

--- Python/AdventureGame/main_game.py
+++ Python/AdventureGame/main_game.py
@@ -1,8 +1,27 @@
 # main build the window and start the program
+import pygame, sys
+from pygame.locals import *
 
+WINDOWWIDTH = 1680
+WINDOWHEIGHT = 1050
 
 def main():
-    print('Hello World')
+    global FPSCLOCK, DISPLAYSURF, BASICFONT
+
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Main Game')
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+        pygame.display.update()
+
+def terminate():
+    '''Terminates games.'''
+    pygame.quit()
+    sys.exit()
 
 if __name__ == '__main__':
     main()
