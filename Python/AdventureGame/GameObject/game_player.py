0700631a05a465408085519b02352b1a552a6656
dislocated-averstra <74801364+dislocated-averstra@users.noreply.github.com>
2020-12-14 21:29:23 -0700

    you can now move in diagonals and I refactored some code to move the player movement into the player class

--- Python/AdventureGame/GameObject/game_player.py
+++ Python/AdventureGame/GameObject/game_player.py
@@ -1,9 +1,11 @@
 class Player:
-    directions = []
+    horizontal_directions = []
+    vertical_directions = []
 
-    def __init__(self, x_position, y_position):
+    def __init__(self, x_position, y_position, size):
         self.x_position = x_position
         self.y_position = y_position
+        self.size = size
 
     def get_x_position(self):
         return self.x_position
@@ -11,30 +13,70 @@ class Player:
     def get_y_position(self):
         return self.y_position
 
-    def get_directions(self):
-        return self.directions
-
     def set_x_position(self, x_position):
         self.x_position = x_position
 
     def set_y_position(self, y_position):
         self.y_position = y_position
 
-    def set_direction(self, move):
-        self.move = move
+    def set_size(self, size):
+        self.size = size
 
-    def add_direction(self, direction):
-       if direction not in self.directions:
-           self.directions.insert(0, direction)
+    def get_size(self):
+        return self.size
 
-    def remove_direction(self, direction):
-        self.directions.remove(direction)
+    def get_horizontal_directions(self):
+        return self.horizontal_directions
+
+    def add_horizontal_direction(self, direction):
+       if direction not in self.horizontal_directions:
+           self.horizontal_directions.insert(0, direction)
+
+    def remove_horizontal_direction(self, direction):
+        self.horizontal_directions.remove(direction)
+
+    def get_vertical_directions(self):
+        return self.vertical_directions
+
+    def add_vertical_direction(self, direction):
+        if direction not in self.vertical_directions:
+            self.vertical_directions.insert(0, direction)
+
+    def remove_vertical_direction(self, direction):
+        self.vertical_directions.remove(direction)
 
     def get_current_direction(self):
-        if len(self.directions) == 0:
-            return None
+        if len(self.horizontal_directions) == 0 and len(self.vertical_directions) == 0:
+            return None, None
+        elif len(self.horizontal_directions) != 0 and len(self.vertical_directions) == 0:
+            return self.horizontal_directions[0], None
+        elif len(self.horizontal_directions) == 0 and len(self.vertical_directions) != 0:
+            return None, self.vertical_directions[0]
         else:
-            return self.directions[0]
+            return self.horizontal_directions[0], self.vertical_directions[0]
+
+    def move_player(self, window_height, window_width):
+        horizontal_direction, vertical_direction = self.get_current_direction()
+        if horizontal_direction is None and vertical_direction is not None:
+            if vertical_direction == 'UP' and self.get_y_position() - 2 > 0:
+                self.set_y_position(self.get_y_position() - 2)
+            if vertical_direction == 'DOWN' and self.get_y_position() + 2 < (window_height - self.size):
+                self.set_y_position(self.get_y_position() + 2)
+        elif horizontal_direction is not None and vertical_direction is None:
+            if horizontal_direction == 'LEFT' and self.get_x_position() - 2 > 0:
+                self.set_x_position(self.get_x_position() - 2)
+            if horizontal_direction == 'RIGHT' and self.get_x_position() + 2 < (window_width - self.size):
+                self.set_x_position(self.get_x_position() + 2)
+        elif horizontal_direction is not None and vertical_direction is not None:
+            if vertical_direction == 'UP' and self.get_y_position() - 1 > 0:
+                self.set_y_position(self.get_y_position() - 1)
+            if vertical_direction == 'DOWN' and self.get_y_position() + 1 < (window_height - self.size):
+                self.set_y_position(self.get_y_position() + 1)
+            if horizontal_direction == 'LEFT' and self.get_x_position() - 1 > 0:
+                self.set_x_position(self.get_x_position() - 1)
+            if horizontal_direction == 'RIGHT' and self.get_x_position() + 1 < (window_width - self.size):
+                self.set_x_position(self.get_x_position() + 1)
+
 
     def to_string(self):
         return 'X position: %s, Y position: %s' % (self.x_position, self.y_position)
--- Python/AdventureGame/main_game.py
+++ Python/AdventureGame/main_game.py
@@ -1,11 +1,11 @@
 # main build the window and start the program
-import pygame
 import sys
+
+import pygame
 from pygame.locals import *
 
-from GameObject.game_player import *
 from GameObject.game_enemy import *
-
+from GameObject.game_player import *
 
 WINDOWWIDTH = 1024
 WINDOWHEIGHT = 768
@@ -24,7 +24,7 @@ DIRECTIONS = ['LEFT', 'RIGHT', ' UP', 'DOWN']
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT, player, enemy
 
-    player = Player(502, 374)
+    player = Player(502, 374, PLAYER_SIZE)
     enemy = Enemy(0,0)
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
@@ -40,40 +40,30 @@ def run_game():
         for event in pygame.event.get():
             if event.type == KEYUP: # a person let go of the key
                 if event.key in (K_a, K_LEFT):
-                    player.remove_direction('LEFT')
+                    player.remove_horizontal_direction('LEFT')
                 if event.key in (K_d, K_RIGHT):
-                    player.remove_direction('RIGHT')
+                    player.remove_horizontal_direction('RIGHT')
                 if event.key in (K_w, K_UP):
-                    player.remove_direction('UP')
+                    player.remove_vertical_direction('UP')
                 if event.key in (K_s, K_DOWN):
-                    player.remove_direction('DOWN')
+                    player.remove_vertical_direction('DOWN')
             if event.type == KEYDOWN: # a person is pressed or is pressing a key
                 if event.key in (K_a, K_LEFT):
-                    player.add_direction('LEFT')
+                    player.add_horizontal_direction('LEFT')
                 if event.key in (K_d, K_RIGHT):
-                    player.add_direction('RIGHT')
+                    player.add_horizontal_direction('RIGHT')
                 if event.key in (K_w, K_UP):
-                    player.add_direction('UP')
+                    player.add_vertical_direction('UP')
                 if event.key in (K_s, K_DOWN):
-                    player.add_direction('DOWN')
+                    player.add_vertical_direction('DOWN')
 
-        move_player(player.get_current_direction())
+        player.move_player(WINDOWHEIGHT, WINDOWWIDTH)
         enemy.move_enemy(player.get_x_position(), player.get_y_position())
         DISPLAYSURF.fill(BGCOLOR)
         draw_player_icon(player.get_x_position(), player.get_y_position(), PLAYER_SIZE, PLAYER_SIZE, RED)
         draw_player_icon(enemy.get_x_position(), enemy.get_y_position(), PLAYER_SIZE, PLAYER_SIZE, BLACK)
         pygame.display.update()
         FPSCLOCK.tick(FPS)
-
-def move_player(move_direction):
-    if move_direction == 'LEFT' and player.get_x_position() - 2 > 0:
-        player.set_x_position(player.get_x_position() - 2)
-    if move_direction == 'RIGHT' and player.get_x_position() + 2 < (WINDOWWIDTH - PLAYER_SIZE):
-        player.set_x_position(player.get_x_position() + 2)
-    if move_direction == 'UP' and player.get_y_position() - 2 > 0:
-        player.set_y_position(player.get_y_position() - 2)
-    if move_direction == 'DOWN' and player.get_y_position() + 2 < (WINDOWHEIGHT - PLAYER_SIZE):
-        player.set_y_position(player.get_y_position() + 2)
 
 def draw_player_icon(x_position, y_position, icon_width_, icon_height, color):
     rect_icon = pygame.Rect(x_position, y_position, icon_width_, icon_height)
