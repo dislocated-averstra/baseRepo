1f94cd8d06dc188d117970f3d23020b65b29fbba
dislocated-averstra <74801364+dislocated-averstra@users.noreply.github.com>
2020-12-19 18:52:13 -0700

    adding super class for game objects

---
+++ Python/AdventureGame/GameObject/game_object.py
@@ -0,0 +1,17 @@
+class GameObject:
+
+    def __init__(self, x_position, y_position):
+        self.x_position = x_position
+        self.y_position = y_position
+
+    def get_x_position(self):
+        return self.x_position
+
+    def get_y_position(self):
+        return self.y_position
+
+    def set_x_position(self, x_position):
+        self.x_position = x_position
+
+    def set_y_position(self, y_position):
+        self.y_position = y_position
