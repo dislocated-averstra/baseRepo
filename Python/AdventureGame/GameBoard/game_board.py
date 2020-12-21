9225796f5ac240ca7339c7fb32318287f9f22bf2
ngocphan90 <bichngoc0803@yahoo.com>
2020-12-20 21:00:42 -0600

    update game display

--- Python/AdventureGame/GameBoard/game_board.py
+++ Python/AdventureGame/GameBoard/game_board.py
@@ -1,15 +1,16 @@
 # this is just a class that will contain the sprites that will need to appear at a given game location
+
 class GameBoard:
 
-    # this will be the board based on the width and height we will add a list to it for each column
+ #this will be the board based on the width and height we will add a list to it for each column
     board = []
 
     def __init__(self, width, height):
-        '''Create the board and store the width and height for future use'''
+      '''Create the board and store the width and height for future use'''
         self.width = width
         self.height = height
         for i in range(0, width):
-            self.board.append([])
+           self.board.append([])
 
     def get_width(self):
         return self.width
@@ -23,5 +24,5 @@ class GameBoard:
     def set_width(self, width):
         self.width = width
 
-    def set_height(self, height):
+   def set_height(self, height):
         self.height = height
