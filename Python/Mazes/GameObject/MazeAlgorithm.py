913f42776d26987e48694fb149e048f6e7704f0d
ngocphan90 <bichngoc0803@yahoo.com>
2021-03-22 11:27:21 -0500

    construction and store staring point in stack

--- Python/Mazes/GameObject/MazeAlgorithm.py
+++ Python/Mazes/GameObject/MazeAlgorithm.py
@@ -6,11 +6,13 @@ from Python.Mazes.GameObject.maze_container import MazeContainer
 
 
 class GenerateMaze:
-    '''list of list for maze'''
+    """ constructor for maze """
 
     def __init__(self, maze_containers):
         self.container = maze_containers
         self.stack = Stack()
+
+    """ store starting point to stack """
 
     def maze_algorithm(self):
         self.stack.push(self.container.maze_wall[0][0])
